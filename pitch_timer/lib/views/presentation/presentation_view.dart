a46f6e5401b0eb73fa6f0f9ceacd7859838eeb75
Elia Bieri <contact@eliabieri.com>
2022-06-28 20:38:04 +0200

    Minor rework

--- pitch_timer/lib/views/presentation/presentation_view.dart
+++ pitch_timer/lib/views/presentation/presentation_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:pitch_timer/models/pitch_chapter.dart';
 import 'package:pitch_timer/models/pitch_data.dart';
@@ -14,7 +15,7 @@ class PresentationView extends StatefulWidget {
 }
 
 class _PresentationViewState extends State<PresentationView> {
-  final double progressIndicatorWidth = 50;
+  static const double progressIndicatorWidth = 50;
 
   DateTime start = DateTime.now();
   int chapterIndex = 0;
@@ -22,10 +23,8 @@ class _PresentationViewState extends State<PresentationView> {
   Duration pauseDuration = Duration.zero;
 
   void skipPrevious(Duration duration) {
-    if (duration.inSeconds < 2) {
-      if (chapterIndex > 0) {
-        chapterIndex = chapterIndex - 1;
-      }
+    if (duration.inSeconds < 2 && chapterIndex > 0) {
+      chapterIndex = chapterIndex - 1;
     }
     start = DateTime.now();
     pauseDuration = Duration.zero;
@@ -57,11 +56,10 @@ class _PresentationViewState extends State<PresentationView> {
         elevation: 5,
       ),
       body: StreamBuilder(
-          stream: Stream.periodic(const Duration(milliseconds: 10),
-              (int _) => DateTime.now().difference(start)),
+          stream: Stream.periodic(
+              const Duration(milliseconds: 10), (int _) => DateTime.now().difference(start)),
           initialData: Duration.zero,
           builder: (context, AsyncSnapshot<Duration> snapshot) {
-            // int? chapterIndex =getChapterIndexForTime(snapshot.data!, widget.pitch.chapters);
             var chapters = widget.pitch.chapters;
             if (!isPaused &&
                 (snapshot.data! > chapters[chapterIndex].duration) &&
@@ -70,16 +68,15 @@ class _PresentationViewState extends State<PresentationView> {
               start = DateTime.now();
             }
 
-            final previousChapter =
-                (chapterIndex) > 0 ? chapters[chapterIndex - 1] : null;
+            final previousChapter = (chapterIndex) > 0 ? chapters[chapterIndex - 1] : null;
             final currentChapter = chapters[chapterIndex];
-            final nextChapter = (chapterIndex) < (chapters.length - 1)
-                ? chapters[chapterIndex + 1]
-                : null;
+            final nextChapter =
+                (chapterIndex) < (chapters.length - 1) ? chapters[chapterIndex + 1] : null;
             return Center(
                 child: Column(
               children: [
                 Container(
+                  height: 90,
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.surface,
                     boxShadow: [
@@ -95,106 +92,96 @@ class _PresentationViewState extends State<PresentationView> {
                     padding: const EdgeInsets.all(10),
                     shrinkWrap: true,
                     children: [
-                      Text("previous",
-                          style: Theme.of(context).textTheme.labelMedium),
-                      Text(previousChapter?.name ?? '',
-                          style: Theme.of(context).textTheme.headlineMedium),
+                      Text("previous", style: Theme.of(context).textTheme.labelMedium),
+                      AutoSizeText(
+                        previousChapter?.name ?? '',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        maxLines: 1,
+                      ),
                     ],
                   ),
                 ),
                 Expanded(
                   child: Container(
-                    color: Theme.of(context)
-                        .colorScheme
-                        .surfaceTint
-                        .withOpacity(0.08),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                    color: Theme.of(context).colorScheme.surfaceTint.withOpacity(0.08),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      Expanded(
+                        child: ListView(
+                          physics: const ClampingScrollPhysics(),
+                          padding: const EdgeInsets.all(25),
+                          shrinkWrap: true,
+                          children: [
+                            const SizedBox(height: 20),
+                            Text(currentChapter.name,
+                                style: Theme.of(context).textTheme.displaySmall),
+                            const SizedBox(height: 20),
+                            Text(currentChapter.notes,
+                                style: Theme.of(context).textTheme.titleLarge),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Stack(
                         children: [
-                          Expanded(
-                            child: ListView(
-                              physics: const ClampingScrollPhysics(),
-                              padding: const EdgeInsets.all(25),
-                              shrinkWrap: true,
-                              children: [
-                                const SizedBox(height: 20),
-                                Text(currentChapter.name,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .displaySmall),
-                                const SizedBox(height: 20),
-                                Text(currentChapter.notes,
-                                    style:
-                                        Theme.of(context).textTheme.titleLarge),
-                                const SizedBox(
-                                  height: 40,
-                                ),
-                              ],
+                          SizedBox(
+                            width: progressIndicatorWidth,
+                            child: RotatedBox(
+                              quarterTurns: 1,
+                              child: LinearProgressIndicator(
+                                minHeight: 20,
+                                color: Theme.of(context).colorScheme.secondary,
+                                value: (isPaused
+                                        ? pauseDuration.inMilliseconds.toDouble()
+                                        : snapshot.data!.inMilliseconds) /
+                                    currentChapter.duration.inMilliseconds,
+                              ),
                             ),
                           ),
-                          Stack(
-                            children: [
-                              SizedBox(
-                                width: progressIndicatorWidth,
-                                child: RotatedBox(
-                                  quarterTurns: 1,
-                                  child: LinearProgressIndicator(
-                                    minHeight: 20,
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                    value: (isPaused
-                                            ? pauseDuration.inMilliseconds
-                                                .toDouble()
-                                            : snapshot.data!.inMilliseconds) /
-                                        currentChapter.duration.inMilliseconds,
+                          Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            width: progressIndicatorWidth,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                GestureDetector(
+                                  onTap: () => skipPrevious(snapshot.data!),
+                                  child: const RotatedBox(
+                                      quarterTurns: 1,
+                                      child: Icon(
+                                        Icons.skip_previous,
+                                        size: progressIndicatorWidth,
+                                      )),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () => pause(snapshot.data!),
+                                    child: Icon(
+                                      isPaused ? Icons.play_arrow : Icons.pause,
+                                      size: progressIndicatorWidth,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 5),
-                                width: progressIndicatorWidth,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () => skipPrevious(snapshot.data!),
-                                      child: RotatedBox(
-                                          quarterTurns: 1,
-                                          child: Icon(
-                                            Icons.skip_previous,
-                                            size: progressIndicatorWidth,
-                                          )),
-                                    ),
-                                    Expanded(
-                                      child: GestureDetector(
-                                        onTap: () => pause(snapshot.data!),
-                                        child: Icon(
-                                          isPaused
-                                              ? Icons.play_arrow
-                                              : Icons.pause,
-                                          size: progressIndicatorWidth,
-                                        ),
-                                      ),
-                                    ),
-                                    GestureDetector(
-                                      onTap: skipNext,
-                                      child: RotatedBox(
-                                          quarterTurns: 1,
-                                          child: Icon(
-                                            Icons.skip_next,
-                                            size: progressIndicatorWidth,
-                                          )),
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ]),
+                                GestureDetector(
+                                  onTap: skipNext,
+                                  child: const RotatedBox(
+                                      quarterTurns: 1,
+                                      child: Icon(
+                                        Icons.skip_next,
+                                        size: progressIndicatorWidth,
+                                      )),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ]),
                   ),
                 ),
                 Container(
+                  height: 90,
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.surfaceVariant,
                     boxShadow: [
@@ -210,10 +197,12 @@ class _PresentationViewState extends State<PresentationView> {
                     padding: const EdgeInsets.all(20),
                     shrinkWrap: true,
                     children: [
-                      Text("next",
-                          style: Theme.of(context).textTheme.labelMedium),
-                      Text(nextChapter?.name ?? '',
-                          style: Theme.of(context).textTheme.headlineMedium),
+                      Text("next", style: Theme.of(context).textTheme.labelMedium),
+                      AutoSizeText(
+                        nextChapter?.name ?? '',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        maxLines: 1,
+                      ),
                     ],
                   ),
                 ),
@@ -234,8 +223,7 @@ class _PresentationViewState extends State<PresentationView> {
     return null;
   }
 
-  Duration getPastChaptersTime(
-      List<PitchChapter> chapters, int? currentChapterIndex) {
+  Duration getPastChaptersTime(List<PitchChapter> chapters, int? currentChapterIndex) {
     Duration timeDelta = Duration.zero;
     for (int index = 0; index != (currentChapterIndex ?? 0); ++index) {
       timeDelta += chapters[index].duration;
